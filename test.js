22ad898a3477fd65e9fee46e839df2bc9b6f8305
Sami Mäkelä <mrsmkl@gmail.com>
2019-03-08 18:47:59 +0200

    fixed error

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ const assert = require('assert')
 const Web3 = require('web3')
 const web3 = new Web3(new Web3.providers.HttpProvider(host))
 
-const getNetwork = require('truebit-utils').getNetwork
+const getNetwork = require('truebit-util').getNetwork
 
 let account, fileSystem, scryptSubmitter
 
